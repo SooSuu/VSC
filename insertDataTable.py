75e1f75040f3a8e4dde93bd4b4493d58644586d1
SooSuu <94596288+SooSuu@users.noreply.github.com>
2021-12-29 18:31:52 +0900

    Add files via upload

---
+++ insertDataTable.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  insertDataTableDHT.py
+#  
+#  Query dada on table "DHT_data" 
+
+import sqlite3 
+
+conn=sqlite3.connect('sensorsData.db') 
+
+curs=conn.cursor()
+ 
+def add_data (temp, hum):
+    curs.execute("INSERT INTO DHT_data values(datetime('now', 'localtime'), (?), (?))", (temp, hum))
+    conn.commit()
+
+add_data (20.5, 30)
+add_data (25.8, 40)
+add_data (30.3, 50)
+
+
+print ("\nEntire database contents:\n")
+for row in curs.execute("SELECT * FROM DHT_data"):
+    print (row)
+
+
+conn.close()
